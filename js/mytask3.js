fb4b9bc0b82ddbdea6b3da95b3ceef5f1b15c499
Jalolov S <jsultonali1993@mail.ru>
2019-12-19 10:05:22 +0500

    Начало работы с слушателями событий

---
+++ js/mytask3.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+
+setupOpen.addEventListener('click', function () {
+    setup.classList.remove('hidden');
+});
+
+setupClose.addEventListener('click', function () {
+    setup.classList.add('hidden');
+});
