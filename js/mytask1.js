d95f85eb5a292d7729a10f9a0de1226e0c079724
Jalolov S <jsultonali1993@mail.ru>
2019-12-20 10:49:05 +0500

    Выполнено начальная работа слушателями событий

--- js/mytask1.js
+++ js/mytask1.js
@@ -1,12 +1,20 @@
-'use strict'
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 240;
+'use strict';
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var START_COORD_X = 100;
-var START_COORD_Y = 50;
+var START_COORD_Y = 10;
 
 var renderCloud = function(ctx,x,y,color){
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(x+10,y+10,CLOUD_WIDTH,CLOUD_HEIGHT);
     ctx.fillStyle = color;
     ctx.fillRect(x,y,CLOUD_WIDTH,CLOUD_HEIGHT);
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.font = "16px 'PT Mono'";
+    ctx.textBaseline = 'hanging';
+
+    ctx.fillText('Ура вы победили!',x+100,y+10);
+    ctx.fillText('Список результатов:',x+100,y+30);
 };
 
 var getMax = function(times){
@@ -23,7 +31,7 @@ window.renderStatistics = function(ctx,players,times){
 
 
     renderCloud(ctx, START_COORD_X, START_COORD_Y, 'rgb(255,255,255)');
-    ctx.font = '24px Tahoma';
+    ctx.font = "16px 'PT Mono'";
 
 
 
@@ -32,12 +40,12 @@ window.renderStatistics = function(ctx,players,times){
     for (var i = 0; i < times.length;i++){
         if (players[i].toUpperCase() === 'Вы'.toUpperCase()){
             ctx.fillStyle = 'rgb(255,0,0)';
-            ctx.fillRect(START_COORD_X+30 + 80*i,START_COORD_Y+CLOUD_HEIGHT-40,50,-((200*times[i]) / maxSize));
-            ctx.fillText(players[i],START_COORD_X+30 + 80*i,START_COORD_Y+CLOUD_HEIGHT-10);
+            ctx.fillRect(START_COORD_X+55 + 90*i,START_COORD_Y+CLOUD_HEIGHT-30,50,-((150*times[i]) / maxSize));
+            ctx.fillText(players[i],START_COORD_X+55 + 90*i,START_COORD_Y+CLOUD_HEIGHT-20);
         } else {
             ctx.fillStyle = 'rgba(0,0,255,' + Math.random() + ')';
-            ctx.fillRect(START_COORD_X+30 + 80*i,START_COORD_Y+CLOUD_HEIGHT-40,50,-((200*times[i]) / maxSize));
-            ctx.fillText(players[i],START_COORD_X+30 + 80*i,START_COORD_Y+CLOUD_HEIGHT-10);
+            ctx.fillRect(START_COORD_X+55 + 90*i,START_COORD_Y+CLOUD_HEIGHT-30,50,-((150*times[i]) / maxSize));
+            ctx.fillText(players[i],START_COORD_X+55 + 90*i,START_COORD_Y+CLOUD_HEIGHT-20);
         }
     }
-}
+};
--- js/mytask2.js
+++ js/mytask2.js
@@ -1,31 +1,32 @@
 'use strict';
 
-var WIZARD_NAMES = ["Дамблдор","Волдеморт","Доктор Стрейдж","Гарри Поттер"];
-
-
-var wizards = [
-    {
-        name: WIZARD_NAMES[0],
-        coatColor: 'rgb(241,43,107)'
-    },
-    {
-        name: WIZARD_NAMES[1],
-        coatColor: 'rgb(215,210,55)'
-    },
-    {
-        name: WIZARD_NAMES[2],
-        coatColor: 'rgb(101,137,164)'
-    },
-    {
-        name: WIZARD_NAMES[3],
-        coatColor: 'rgb(127,127,127)'
-    },
+var NAMES = ["Иван","Хуан Себастьян","Мария","Кристоф","Виктор","Юлия","Люпита","Вашингтон"];
+var FAMILIES = ["да Марья","Верон","Мирабелла","Вальц","Онопко","Топольницкая","Нионго","Ирвинг"];
+var COLORS = [
+"rgb(101, 137, 164)",
+    "rgb(241, 43, 107)",
+    "rgb(146, 100, 161)",
+    "rgb(56, 159, 117)",
+    "rgb(215, 210, 55)",
+    "rgb(0, 0, 0)"
 ];
+
+var generateWizards = function(count){
+    var wizards = [];
+    for (var i = 0; i < count; i++){
+        wizards[i] = {
+            wizardName: NAMES[Math.floor(Math.random()*7)] + " " + FAMILIES[Math.floor(Math.random()*7)],
+            coatColor: COLORS[Math.floor(Math.random()*5)],
+        };
+
+    }
+    return wizards;
+};
+
+var wizards = generateWizards(4);
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -34,7 +35,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 var renderWizard = function(wizard){
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.wizardName;
     wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
     return wizardElement;
 };
